8bd415a6f0a92dff6b971f55e64b05ef01d379ca
Antonio Ayala <arcadia501@gmail.com>
2022-05-06 18:17:18 -0700

    [79. Formulario Tipo Cuentas]

---
+++ ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -0,0 +1,18 @@
+﻿using ManejoPresupuesto.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class TiposCuentasController: Controller
+    {
+        public IActionResult Crear()
+        {
+            return View(); 
+        }
+        [HttpPost]
+        public IActionResult Crear(TipoCuenta tipoCuenta)
+        {
+            return View();
+        }
+    }
+}
