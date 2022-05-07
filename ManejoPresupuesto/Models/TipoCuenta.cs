0fe06df77388d9fe7df4dd3abeefd59a6e74bc54
Antonio Ayala <arcadia501@gmail.com>
2022-05-06 19:27:41 -0700

    [m][81. Visualizando los Errores de Validación]

--- ManejoPresupuesto/Models/TipoCuenta.cs
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -5,7 +5,8 @@ namespace ManejoPresupuesto.Models
     public class TipoCuenta
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage ="El campo {0} es requerido")]
+        [StringLength(maximumLength:50, MinimumLength =3,ErrorMessage ="Largo {1} y {2}")]
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
         public int Orden { get; set; }
