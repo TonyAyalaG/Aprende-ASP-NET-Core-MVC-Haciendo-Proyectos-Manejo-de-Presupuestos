74a93eeb21dbfd6de2ec2c45615298470c3f5825
Antonio Ayala <arcadia501@gmail.com>
2022-05-07 11:44:58 -0700

    [m][83. Otras Validaciones por Defecto]

--- ManejoPresupuesto/Models/TipoCuenta.cs
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -11,5 +11,19 @@ namespace ManejoPresupuesto.Models
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
         public int Orden { get; set; }
+
+        /* Pruebas de otras validaciones por defecto*/
+        [Required(ErrorMessage ="El campo {0} es requerido")]
+        [EmailAddress(ErrorMessage ="El campo debe ser un correo electronico valido")]
+        public string Email { get; set; }
+        [Range(minimum:18,maximum:130,ErrorMessage ="El valor debe de estar entre {1} y {2}")]
+        public int Edad { get; set; }
+        [Url(ErrorMessage ="El campo debe ser una URL valida")]
+        public string URL { get; set; }
+        [CreditCard(ErrorMessage ="La tarjeta de credito no es valida")]
+        [Display(Name ="Tarjeta de Credito")]
+        public string TarjetaDeCredito { get; set; }
+
+
     }
 }
