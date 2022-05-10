a72b48a06acb80660f84b9abde599d2d3341521c
Antonio Ayala <arcadia501@gmail.com>
2022-05-10 13:15:01 -0700

    [105. Insertar Cuenta]

---
+++ ManejoPresupuesto/Servicios/RepositorioCuentas.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using ManejoPresupuesto.Models;
+using Microsoft.Data.SqlClient;
+
+namespace ManejoPresupuesto.Servicios
+{
+    public interface IRepositorioCuentas
+    {
+        Task Crear(Cuenta cuenta);
+    }
+    public class RepositorioCuentas: IRepositorioCuentas
+    {
+        private readonly string connectionString;
+        public RepositorioCuentas(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+                            
+        }
+
+        public async Task Crear(Cuenta cuenta)
+        {
+            using var connection = new  SqlConnection(connectionString);
+            var id = await connection.QuerySingleAsync<int>(@"Insert Into Cuentas
+                                                (Nombre, TipoCuentaId, Descripcion, Balance)
+                                          Values (@Nombre, @TipoCuentaId, @Descripcion, @Balance);
+                                        Select SCOPE_IDENTITY();", cuenta);
+            cuenta.Id = id;
+        }
+    }
+}
